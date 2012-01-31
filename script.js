1274877d2b8ce18f704add6537f77fa32663f7c6
Leigh Zhu <zhulime@gmail.com>
2012-01-31 23:09:33 +0800

    gh-pages-commit-all

---
+++ script.js
@@ -0,0 +1,14 @@
+function externallinks() {
+    if(!document.getElementsByTagName)
+        return;
+    var links = document.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++) {
+        var link = links[i];
+        if(link.getAttribute("href"))
+            link.target = "_blank";
+    }
+}
+
+window.onload = function() {
+    externallinks();
+}
