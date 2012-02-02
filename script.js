bb68fe6f8a007e0856894c9ff75b8e910c9efca4
Leigh Zhu <zhulime@gmail.com>
2012-02-02 20:12:16 +0800

    add tos en lang,add a.rel=external,modify script for links open, some links fix.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function externallinks() {
     var links = document.getElementsByTagName("a");
     for(var i = 0; i < links.length; i++) {
         var link = links[i];
-        if(link.getAttribute("href") != "http://kindle.silubeta.com")
+        if(anchor.getAttribute("href") && anchor.getAttribute("rel") == "external")
             link.target = "_blank";
     }
 }
