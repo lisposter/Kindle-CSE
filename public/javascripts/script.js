6655936eaaea8536cbd922c210d3eeb431d6f6bc
Leigh <i@zhuli.me>
2012-07-20 14:06:44 +0800

    fix the "/" link

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,15 @@
 function openLinksInNewWindow() {
-    var hostName = document.location.href.match(/^(http:\/\/)?([^\/]+)/i)[2];
-    if (! document.getElementsByTagName) return;
-    var links = document.getElementsByTagName("a");
-    for (var i = links.length - 1; i >= 0; i--) {
-        if (links[i].getAttribute("href").indexOf(hostName) == -1) {
-            links[i].setAttribute("target", "_blank");
-        };
-    };
+	var hostName = document.location.href.match(/^(http:\/\/)?([^\/]+)/i)[2];
+	if (! document.getElementsByTagName) return;
+	var links = document.getElementsByTagName("a");
+	for (var i = links.length - 1; i >= 0; i--) {
+		href = links[i].getAttribute("href");
+		if (href.indexOf(hostName) == -1 && href.indexOf("http") >= 0) {
+			links[i].setAttribute("target", "_blank");
+		};
+	};
 }
 
 window.onload = function() {
-    openLinksInNewWindow();
+	openLinksInNewWindow();
 }
